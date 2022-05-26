7fc3dc9d61b90ac09a1aca8ab83844414a15a92d
Dmytro-Makarov <makarowdoua@gmail.com>
2022-05-26 22:04:21 +0300

    please

---
+++ Controllers/ImageConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace GameLibWeb.Controllers;
+
+public static class ImageConverter
+{
+    public static Image LoadBase64(string base64)
+    {
+        byte[] bytes = Convert.FromBase64String(base64);
+        Image image;
+        using (MemoryStream ms = new MemoryStream(bytes))
+        {
+            image = Image.FromStream(ms);
+        }
+        return image;
+    }
+
+    public static string ToBase64(IFormFile image)
+    {
+        using MemoryStream ms = new MemoryStream();
+        image.CopyTo(ms);
+        byte[] imageBytes = ms.ToArray();
+
+        string base64String = Convert.ToBase64String(imageBytes);
+        return base64String;
+    }
+}
